6b6356b1a535e51b299aca3518f6e8c60053fd23
Scott Sullivan <scottmsul@gmail.com>
2024-12-16 10:23:29 -0500

    This is a major update the logically separates the solver code from the printing code. The idea is that the solver code should return the solver results, but has no side effects and is agnostic about how the results should be displayed. This is a necessary update to prepare the codebase for much improved user interfaces such as a web app or python gui (currently planning on web app). This update is also backwards-compatible with existing scripts, so the code can still be run from the command line if desired.

--- scripts/flow_chart.py
+++ scripts/flow_chart.py
@@ -26,7 +26,7 @@ class FlowChartGenerator:
                 data = linear_solver.parse_recipe_id(recipe_var.name())
                 data['solution_value'] = recipe_var.solution_value()
                 
-                recipe_name = data['recipe_name']
+                recipe_name = data['recipe_key']
                 if data['machine'] == 'recycler':
                     recipe_name = recipe_name.replace('-recycling', '')
                     
@@ -61,7 +61,7 @@ class FlowChartGenerator:
         for variant in variant_list:
             # Get localized names
             machine = self.get_localized_name(variant['machine'], self.items)
-            name = self.get_localized_name(variant['recipe_name'], self.recipes)
+            name = self.get_localized_name(variant['recipe_key'], self.recipes)
             
             # Format modules string
             mods = self.format_modules(variant['num_qual_modules'], 
@@ -96,6 +96,8 @@ class FlowChartGenerator:
 
     def write_flow_chart(self, output_flow_chart):
         """Generate and save a Mermaid flow chart of the recipe system."""
+        print('')
+        print(f'Writing flow chart to: {output_flow_chart}')
         variants = self.collect_recipe_variants()
         classes = defaultdict(list)
         
--- scripts/test_examples.py
+++ scripts/test_examples.py
@@ -1,16 +1,20 @@
 import json
 import os
-from linear_solver import LinearSolver
+from linear_solver import run_solver_from_command_line
 
 def main():
     codebase_path = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
     examples_path = os.path.join(codebase_path, 'examples')
     for example_config in os.listdir(examples_path):
         example_config_filename = os.path.join(examples_path, example_config)
-        with open(example_config_filename) as f:
-            example_config_data = json.load(f)
-        solver = LinearSolver(config=example_config_data)
-        solver.run()
+
+        with open(example_config_filename ) as config_file:
+            config = json.load(config_file)
+
+        with open(config['data']) as data_file:
+            data = json.load(data_file)
+
+        run_solver_from_command_line(config, data)
 
 if __name__=='__main__':
     main()
